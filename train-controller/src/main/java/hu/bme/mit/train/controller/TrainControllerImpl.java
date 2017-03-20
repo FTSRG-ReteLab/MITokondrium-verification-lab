f18e7fe9cf2984cd6d3ddc2c6ae19c0b78f2e37d
Tayaki <ugromokus93@gmail.com>
2017-03-20 07:15:36 -0700

    Reference speed changing after every 5 sec, if the user change the joystick position.

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,18 +7,10 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
-
+	
 	@Override
 	public void followSpeed() {
-		if (referenceSpeed < 0) {
-			referenceSpeed = 0;
-		} else {
-		    if(referenceSpeed+step > 0) {
-                referenceSpeed += step;
-            } else {
-		        referenceSpeed = 0;
-            }
-		}
+		referenceSpeed = referenceSpeed + step;
 
 		enforceSpeedLimit();
 	}
@@ -34,10 +26,14 @@ public class TrainControllerImpl implements TrainController {
 		enforceSpeedLimit();
 		
 	}
-
+	
 	private void enforceSpeedLimit() {
 		if (referenceSpeed > speedLimit) {
 			referenceSpeed = speedLimit;
+		}
+		
+		if (referenceSpeed < 0) {
+			referenceSpeed = 0;
 		}
 	}
 
