9b12bd76b916f8030aaaacc33b5d20c7eaf3ca81
taszilvo <taszilvo@gmail.com>
2017-03-20 07:14:18 -0700

    Reference speed full implemented

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -7,12 +7,13 @@ import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
 
-public class TrainSystem {
+public class TrainSystem implements Runnable {
 
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
 
+	
 	public TrainController getController() {
 		return controller;
 	}
@@ -23,6 +24,17 @@ public class TrainSystem {
 
 	public TrainUser getUser() {
 		return user;
+	}
+
+	@Override
+	public void run() {
+		controller.followSpeed();
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
